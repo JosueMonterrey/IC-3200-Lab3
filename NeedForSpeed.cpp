4dfcebde349f77fbc2e28283eb62d1052fa76be3
JosueMonterrey <josuemr0206@gmail.com>
2026-04-22 20:00:12 -0600

    Need For Speed resuelto.

--- NeedForSpeed.cpp
+++ NeedForSpeed.cpp
@@ -1,9 +1,44 @@
 // https://open.kattis.com/problems/speed
 
 #include <bits/stdc++.h>
+#define vi vector<int>
 
 using namespace std;
 
+double calc(vi& D, vi& S, const int N, const double c) {
+    double r = 0.00;
+    for (int i = 0; i < N; i++)
+        r += D[i] / (S[i] + c);
+    
+    return r;
+}
+
 int main() {
+
+    int N, T;
+    cin >> N >> T;
+
+    vi D(N), S(N);
+    int minS = 10000;
+    for (int i = 0; i < N; i++) {
+        cin >> D[i] >> S[i];
+        if (S[i] < minS)
+            minS = S[i];
+    }
+    
+    double inf = -minS, sup = 2000000.0;
+
+    while (sup - inf >= 1e-6) {
+        double c = (sup + inf) / 2;
+
+        double t = calc(D, S, N, c);
+        if (t > T)
+            inf = c;
+        else
+            sup = c;
+    }
+
+    cout << fixed << setprecision(9) << sup << endl;
+
     return 0;
 }
