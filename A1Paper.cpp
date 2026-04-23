de5a6761fda69f8cb8348264c3fb9457a073faed
JosueMonterrey <josuemr0206@gmail.com>
2026-04-23 13:09:23 -0600

    A1 Paper resuelto.

--- A1Paper.cpp
+++ A1Paper.cpp
@@ -1,9 +1,47 @@
 // https://open.kattis.com/problems/a1paper
 
 #include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define A2_LONG_SIDE 0.5946035575
+#define A2_SHORT_SIDE 0.4204482076
 
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
+
+    vi A(N - 1);
+    for (int& a : A)
+        cin >> a;
+    
+    
+    double tape = 0.0;
+    bool isPossible = false;
+    int needed = 2;
+    double longSide = A2_LONG_SIDE, shortSide = A2_SHORT_SIDE;
+    
+    for (int i = 0; i < N; i++) {
+        tape += (needed / 2) * longSide;
+
+        ll available = A[i];
+        if (available >= needed) {
+            isPossible = true;
+            break;
+        }
+
+        needed = (needed - available) * 2;
+
+        double temp = longSide;
+        longSide = shortSide;
+        shortSide = temp / 2.0;
+    }
+
+    if (isPossible)
+        cout << fixed << setprecision(9) << tape << endl;
+    else
+        cout << "impossible" << endl;
+
     return 0;
 }
